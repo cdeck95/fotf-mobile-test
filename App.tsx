6fbd5b7b9b43187b1a66ce722c76de6434380cf2
Chris Deck <deckchris95@gmail.com>
2023-04-14 07:28:24 -0400

    webview works

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const MyWebComponent = () => {
+  return <WebView source={{ uri: 'https://app.furyofthefur.com' }} style={styles.webview} />
+}
+
+const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get('window').width;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MyWebComponent/>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    width: "100%", height: "100%"
+  }, webview: {
+    marginTop: 35,
+    width: deviceWidth,
+    height: deviceHeight
+
+  }
 });
