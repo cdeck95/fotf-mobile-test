742940b9fe501f916721c8bdda26238f0877ecd5
Chris Deck <deckchris95@gmail.com>
2023-07-03 12:01:49 -0400

    small changes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import { StatusBar, StatusBarStyle } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions, SafeAreaView} from 'react-native';
 import { WebView } from 'react-native-webview';
+
+const STYLES = ['default', 'dark-content', 'light-content'] as const;
+const TRANSITIONS = ['fade', 'slide', 'none'] as const;
 
 const MyWebComponent = () => {
   return <WebView source={{ uri: 'https://poetic-bonbon-daf9f3.netlify.app/' }} style={styles.webview} />
 }
 
+// const [hidden, setHidden] = useState(false);
+// const [statusBarStyle, setStatusBarStyle] = useState<StatusBarStyle>(
+//     STYLES[0],
+//   );
+//   const [statusBarTransition, setStatusBarTransition] = useState<
+//     'fade' | 'slide' | 'none'
+//   >(TRANSITIONS[0]);
+//   const changeStatusBarStyle = () => {
+//     const styleId = STYLES.indexOf(statusBarStyle) + 1;
+//     if (styleId === STYLES.length) {
+//       setStatusBarStyle(STYLES[0]);
+//     } else {
+//       setStatusBarStyle(STYLES[styleId]);
+//     }
+//   };
+
+//   const changeStatusBarTransition = () => {
+//     const transition = TRANSITIONS.indexOf(statusBarTransition) + 1;
+//     if (transition === TRANSITIONS.length) {r
+//       setStatusBarTransition(TRANSITIONS[0]);
+//     } else {
+//       setStatusBarTransition(TRANSITIONS[transition]);
+//     }
+//   };
+  
 const deviceHeight = Dimensions.get('window').height;
 const deviceWidth = Dimensions.get('window').width;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <MyWebComponent/>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+          animated={true}
+          backgroundColor="#61dafb"
+          style="light"
+          // hidden={hidden}
+        />
+      <View style={styles.container}>
+      
+        <MyWebComponent/>
+      </View>
+    </SafeAreaView>
     
   );
 }
@@ -26,11 +62,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
-    width: "100%", height: "100%"
+    width: "100%", height: "100%",
   }, webview: {
-    marginTop: 45,
     width: deviceWidth,
     height: deviceHeight
-
   }
 });
